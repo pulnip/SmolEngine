382bfe5215e6a9e485f27411ad501b11c2496677
pulnip <choiwh1008@gmail.com>
2026-05-18 10:07:59 +0900

    rename move_cast to unique_cast

--- Engine/Reflection/Private/ActorFactory.cpp
+++ Engine/Reflection/Private/ActorFactory.cpp
@@ -8,8 +8,9 @@ namespace Smol
 
     template <typename Base, typename Derived>
         requires std::is_base_of_v<Base, Derived>
-    RAII<Derived> move_cast(RAII<Base>&& base){
-        if(Derived* raw = dynamic_cast<Derived*>(base.release())){
+    RAII<Derived> unique_cast(RAII<Base>& base){
+        if(Derived* raw = dynamic_cast<Derived*>(base.get())){
+            base.release();
             return RAII<Derived>(raw);
         }
 
@@ -17,6 +18,7 @@ namespace Smol
     }
 
     ActorRAII CreateActor(StrView name){
-        return move_cast<Object, Actor>(CreateObject(name));
+        auto object = CreateObject(name);
+        return unique_cast<Object, Actor>(object);
     }
 }
