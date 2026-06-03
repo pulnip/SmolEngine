1a3b3e5b14f8c3ea24f7b1ad1f903700cccfbe3f
pulnip <choiwh1008@gmail.com>
2026-06-03 12:55:41 +0900

    add sprite animation sample

---
+++ Engine/Render/Sample/SpriteRendererAnimSample.cpp
@@ -0,0 +1,107 @@
+#include <array>
+#include "ImageLoader.hpp"
+#include "Primitives.hpp"
+#include "RHICommandList.hpp"
+#include "RHIDevice.hpp"
+#include "RHISwapchain.hpp"
+#include "RHITexture.hpp"
+#include "RuntimeConfig.hpp"
+#include "SpriteAnimation.hpp"
+#include "SpriteRenderer.hpp"
+#include "Timer.hpp"
+#include "Window.hpp"
+
+using namespace Smol;
+
+CStr IMAGE_PATH = "Content/Assets/Sprite/hollow_knight.png";
+
+int main(void){
+    auto device = CreateDevice();
+    WindowConfig windowConfig{
+        .title = "TextureSample",
+        .width = 800, .height = 800,
+        .fullscreen = false,
+        .resizable = false,
+    };
+    Window window(windowConfig);
+    auto fWidth = static_cast<f32>(window.GetWidth());
+    auto fHeight = static_cast<f32>(window.GetHeight());
+
+    RHITextureCreateDesc backBufferDesc{
+        .width = windowConfig.width, .height = windowConfig.height,
+        .format = RHIPixelFormat::RGBA8_UNORM
+    };
+    auto swapchain = device->CreateSwapchain(RHISwapchainCreateDesc{
+        .sdlWindow = window.GetWindow(),
+        .bufferDesc = backBufferDesc
+    });
+
+    // load Texture from Image
+    auto image = loadImage(IMAGE_PATH);
+    auto texture = device->CreateTexture(RHITextureCreateDesc{
+        .width = image.GetWidth(), .height = image.GetHeight(),
+        .format = RHIPixelFormat::RGBA8_UNORM,
+        .usage = RHITextureUsage::AllowShaderRead,
+        .initialData = image.GetBufferPointer()
+    });
+
+    RHIClearColor clearColor{.v = {0.5f, 0.5f, 0.5f, 1.0f}};
+    auto cmdList = device->CreateCommandList();
+
+    SpriteRenderer renderer(*device);
+
+    Timer timer;
+    constexpr f32 framePerSeconds = 0.16f;
+    constexpr u32 numFrames = 8;
+
+    f32 et = 0.0f;
+    u32 animFrame = 0;
+
+    while(true){
+        if(!window.ProcessEvents()) [[unlikely]]
+            break;
+
+        timer.NewFrame();
+        et += timer.GetDeltaTime();
+        if(et > framePerSeconds){
+            et -= framePerSeconds;
+            animFrame = (animFrame + 1) % numFrames;
+        }
+
+        cmdList->Begin();
+        cmdList->BeginRenderPass(*swapchain,
+            clearColor,
+            nullptr,
+            {},
+            RHILoadAction::Clear
+        );
+
+        cmdList->SetViewport(RHIViewport{
+            .x = 0, .y = 0,
+            .width = fWidth, .height = fHeight,
+            .minDepth = 0, .maxDepth = 1
+        });
+
+        std::array items = {
+            SpriteRenderItem{
+                .uvScale = {.x = 1.0f/16, .y = 1.0f/16},
+                .offset = {
+                    .x = static_cast<f32>(animFrame),
+                    .y = 0
+                },
+                .texture = *texture
+            }
+        };
+
+        renderer.Draw(*cmdList, items);
+
+        cmdList->EndRenderPass();
+        cmdList->Close();
+
+        device->Submit(*cmdList, swapchain.get());
+    }
+
+    cmdList->WaitUntilCompleted();
+
+    return 0;
+}
