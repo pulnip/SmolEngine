2c2682bfd5ead38adf016e09bd29e9d5110592cb
pulnip <choiwh1008@gmail.com>
2026-06-06 17:40:00 +0900

    enhance Reflection support for primitive type

--- Engine/Reflection/Private/TypeTraits.cpp
+++ Engine/Reflection/Private/TypeTraits.cpp
@@ -8,6 +8,18 @@ namespace Smol
         }
     }
 
+    void TypeTraits<i8>::deserialize(void* data, const DOM::Value& value){
+        if(auto v = value.asInt()){
+            *static_cast<i8*>(data) = static_cast<i8>(*v);
+        }
+    }
+
+    void TypeTraits<i16>::deserialize(void* data, const DOM::Value& value){
+        if(auto v = value.asInt()){
+            *static_cast<i16*>(data) = static_cast<i16>(*v);
+        }
+    }
+
     void TypeTraits<i32>::deserialize(void* data, const DOM::Value& value){
         if(auto v = value.asInt()){
             *static_cast<i32*>(data) = static_cast<i32>(*v);
@@ -17,6 +29,30 @@ namespace Smol
     void TypeTraits<i64>::deserialize(void* data, const DOM::Value& value){
         if(auto v = value.asInt()){
             *static_cast<i64*>(data) = *v;
+        }
+    }
+
+    void TypeTraits<u8>::deserialize(void* data, const DOM::Value& value){
+        if(auto v = value.asInt()){
+            *static_cast<u8*>(data) = static_cast<u8>(*v);
+        }
+    }
+
+    void TypeTraits<u16>::deserialize(void* data, const DOM::Value& value){
+        if(auto v = value.asInt()){
+            *static_cast<u16*>(data) = static_cast<u16>(*v);
+        }
+    }
+
+    void TypeTraits<u32>::deserialize(void* data, const DOM::Value& value){
+        if(auto v = value.asInt()){
+            *static_cast<u32*>(data) = static_cast<u32>(*v);
+        }
+    }
+
+    void TypeTraits<u64>::deserialize(void* data, const DOM::Value& value){
+        if(auto v = value.asInt()){
+            *static_cast<u64*>(data) = static_cast<u64>(*v);
         }
     }
 
@@ -35,6 +71,31 @@ namespace Smol
     void TypeTraits<Str>::deserialize(void* data, const DOM::Value& value){
         if(auto v = value.asString()){
             *static_cast<Str*>(data) = *v;
+        }
+    }
+
+    void TypeTraits<Size2D>::deserialize(void* data, const DOM::Value& value){
+        auto& size = *static_cast<Size2D*>(data);
+
+        if(auto v = value.get<u32>("x"); v.has_value()){
+            size.x = *v;
+        }
+        if(auto v = value.get<u32>("y"); v.has_value()){
+            size.y = *v;
+        }
+    }
+
+    void TypeTraits<Transform>::deserialize(void* data, const DOM::Value& value){
+        auto& transform = *static_cast<Transform*>(data);
+
+        if(auto v = value.get<Vec3>("position"); v.has_value()){
+            transform.position = *v;
+        }
+        if(auto v = value.get<Vec4>("rotation"); v.has_value()){
+            transform.rotation = *v;
+        }
+        if(auto v = value.get<Vec3>("scale"); v.has_value()){
+            transform.scale = *v;
         }
     }
 }
--- Engine/Reflection/Public/TypeInfo.hpp
+++ Engine/Reflection/Public/TypeInfo.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "Primitives.hpp"
 #include "DOM.hpp"
+#include "Primitives.hpp"
 
 namespace Smol
 {
@@ -17,39 +17,98 @@ namespace Smol
     template<>
     struct TypeTraits<bool>{
         static constexpr CStr name = "bool";
-        static void deserialize(void* data, const DOM::Value& value);
+        static void deserialize(void*, const DOM::Value&);
+    };
+
+    template<>
+    struct TypeTraits<i8>{
+        static constexpr CStr name = "i8";
+        static void deserialize(void*, const DOM::Value&);
+    };
+
+    template<>
+    struct TypeTraits<i16>{
+        static constexpr CStr name = "i16";
+        static void deserialize(void*, const DOM::Value&);
     };
 
     template<>
     struct TypeTraits<i32>{
         static constexpr CStr name = "i32";
-        static void deserialize(void* data, const DOM::Value& value);
+        static void deserialize(void*, const DOM::Value&);
     };
 
     template<>
     struct TypeTraits<i64>{
         static constexpr CStr name = "i64";
-        static void deserialize(void* data, const DOM::Value& value);
+        static void deserialize(void*, const DOM::Value&);
+    };
+
+    template<>
+    struct TypeTraits<u8>{
+        static constexpr CStr name = "u8";
+        static void deserialize(void*, const DOM::Value&);
+    };
+
+    template<>
+    struct TypeTraits<u16>{
+        static constexpr CStr name = "u16";
+        static void deserialize(void*, const DOM::Value&);
+    };
+
+    template<>
+    struct TypeTraits<u32>{
+        static constexpr CStr name = "u32";
+        static void deserialize(void*, const DOM::Value&);
+    };
+
+    template<>
+    struct TypeTraits<u64>{
+        static constexpr CStr name = "u64";
+        static void deserialize(void*, const DOM::Value&);
+    };
+
+    template<>
+    struct TypeTraits<usize>{
+        static constexpr CStr name = "usize";
+        static void deserialize(void*, const DOM::Value&);
+    };
+
+    template<>
+    struct TypeTraits<isize>{
+        static constexpr CStr name = "isize";
+        static void deserialize(void*, const DOM::Value&);
     };
 
     template<>
     struct TypeTraits<f32>{
         static constexpr CStr name = "f32";
-        static void deserialize(void* data, const DOM::Value& value);
+        static void deserialize(void*, const DOM::Value&);
     };
 
     template<>
     struct TypeTraits<f64>{
         static constexpr CStr name = "f64";
-        static void deserialize(void* data, const DOM::Value& value);
+        static void deserialize(void*, const DOM::Value&);
     };
 
     template<>
     struct TypeTraits<Str>{
         static constexpr CStr name = "Str";
-        static void deserialize(void* data, const DOM::Value& value);
+        static void deserialize(void*, const DOM::Value&);
     };
 
+    template<>
+    struct TypeTraits<Size2D>{
+        static constexpr CStr name = "Size2D";
+        static void deserialize(void*, const DOM::Value&);
+    };
+
+    template<>
+    struct TypeTraits<Transform>{
+        static constexpr CStr name = "Transform";
+        static void deserialize(void*, const DOM::Value&);
+    };
 
     template<typename T>
     const TypeInfo* GetTypeInfo(){
