b5cd756d8ad5b93ea8c8c3a2e3a8b08d5c90bd9b
pulnip <choiwh1008@gmail.com>
2026-06-30 11:56:09 +0900

    add hash for core and rhi types

--- Engine/Core/Public/HashUtil.hpp
+++ Engine/Core/Public/HashUtil.hpp
@@ -15,3 +15,48 @@ namespace Smol
         return h;
     }
 }
+
+template<>
+struct std::hash<Smol::Vec2>{
+    std::size_t operator()(const Smol::Vec2& v) const noexcept{
+        using namespace Smol;
+
+        return hashAll(v.x, v.y);
+    }
+};
+
+template<>
+struct std::hash<Smol::Vec3>{
+    std::size_t operator()(const Smol::Vec3& v) const noexcept{
+        using namespace Smol;
+
+        return hashAll(v.x, v.y, v.z);
+    }
+};
+
+template<>
+struct std::hash<Smol::Vec4>{
+    std::size_t operator()(const Smol::Vec4& v) const noexcept{
+        using namespace Smol;
+
+        return hashAll(v.x, v.y, v.z, v.w);
+    }
+};
+
+template<>
+struct std::hash<Smol::Size2D>{
+    std::size_t operator()(const Smol::Size2D& s) const noexcept{
+        using namespace Smol;
+
+        return hashAll(s.x, s.y);
+    }
+};
+
+template<>
+struct std::hash<Smol::Size3D>{
+    std::size_t operator()(const Smol::Size3D& s) const noexcept{
+        using namespace Smol;
+
+        return hashAll(s.x, s.y, s.z);
+    }
+};
